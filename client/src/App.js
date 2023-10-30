ca19885e0f84150f60b2d2f8f59bd8142a650626
bishal-dd <dhakalbishal930@gmail.com>
2023-10-30 21:56:33 +0600

    user add complete

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,15 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
+        <ToastContainer position="top-center" />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/sign_up" element={<SignUp />} />
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,21 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify"; // For displaying toasts
+import { useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({
+    user_name: "",
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    // Construct your GraphQL query
+    const query = `
+        mutation SignUp($user_name: String!, $email: String!, $password: String!) {
+          createUser(user_name: $user_name, email: $email, password: $password) {
+            user_name
+          }
+        }
+    `;
+
+    const variables = {
+      user_name: formData.user_name,
+      email: formData.email,
+      password: formData.password,
+    };
+    try {
+      await axios.post(`${process.env.REACT_APP_GRAPHQL_URL}`, {
+        query,
+        variables,
+      });
+
+      //
+      toast.success("Sign up successful!");
+      navigate("/sign_in");
+    } catch (error) {
+      // Handle the error here
+      console.error("Sign up error:", error);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    console.log(formData);
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="bg-white w-96 p-6 rounded-lg shadow-lg border  border-gray-300">
         <h2 className="text-2xl font-bold mb-4">Sign Up</h2>
-        <form>
+        <form autoComplete="off" onSubmit={handleFormSubmit}>
           <div className="mb-4">
             <label htmlFor="name" className="block text-gray-600 font-medium">
               Name
             </label>
             <input
               type="text"
-              id="name"
-              name="name"
+              id="user_name"
+              name="user_name"
               className="w-full border border-gray-300 rounded p-2"
               placeholder="Your Name"
+              onChange={handleInputChange}
             />
           </div>
           <div className="mb-4">
@@ -28,6 +76,7 @@ export default function SignUp() {
               name="email"
               className="w-full border border-gray-300 rounded p-2"
               placeholder="Your Email"
+              onChange={handleInputChange}
             />
           </div>
           <div className="mb-4">
@@ -43,6 +92,7 @@ export default function SignUp() {
               name="password"
               className="w-full border border-gray-300 rounded p-2"
               placeholder="Password"
+              onChange={handleInputChange}
             />
           </div>
           <button
