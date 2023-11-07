cbbf2bd694453b76969cfb81e08635c961335d42
bishal-dd <dhakalbishal930@gmail.com>
2023-11-07 15:50:32 +0600

    express server back

--- server/server.js
+++ server/server.js
@@ -1,35 +1,38 @@
+const bodyParser = require("body-parser");
 const express = require("express");
-const { ApolloServer } = require("apollo-server-express");
-const cors = require("cors");
-const db = require("./models");
-const schema = require("./api/graphql");
-
-const port = 8080;
-
 const app = express();
-
+const cors = require("cors");
+var { graphqlHTTP } = require("express-graphql");
+var { buildSchema, GraphQLSchema } = require("graphql");
+const { expressjwt: jwt } = require("express-jwt");
+const db = require("./models");
+const schema = require("./schemas/index");
+const port = 8080;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 require("dotenv").config();
 
-const server = new ApolloServer({
-  schema,
-  context: ({ req }) => {
-    // You can add context data here if needed
-  },
-  cache: "bounded",
-});
+app.use(
+  "/graphql",
+  jwt({
+    secret: process.env.JWT_SECRET,
+    algorithms: ["HS256"], // Algorithm used to sign the token
+  }).unless({
+    path: ["/graphql", "/contact", "/sign_up"], // Add routes that don't require authentication
+  })
+);
 
-async function startServer() {
-  await server.start();
-  server.applyMiddleware({ app, path: "/graphql" });
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 
+db.sequelize.sync().then((req) => {
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
-}
-
-db.sequelize.sync().then(() => {
-  startServer();
 });
